ec96dcf6246a6fff6623821b9538e4a0b6b89b6c
Kotalov <kotalov.developer@gmail.com>
2024-11-26 09:14:16 +0100

    WW-6: Prepare assets (fonts, icons, colors) (#3)
    * WW-2: Prepare assets (fonts, icons, colors)
    
    * Add gitignore to ignore .DS_Store and .xcuserstate
    
    * fix/WW-6/fix code view

--- WeWatch/WeWatch/ContentView.swift
+++ WeWatch/WeWatch/ContentView.swift
@@ -10,12 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+            
             Text("Hello, world!")
+                .font(.poppinsBold30px)
+            Image(.homeActiveIcon)
         }
-        .padding()
     }
 }
 
---
+++ WeWatch/WeWatch/Preview Content/Assets/Font/Font extension.swift
@@ -0,0 +1,33 @@
+//
+//  Font extension.swift
+//  WeWatch
+//
+//  Created by Anton on 25/11/2024.
+//
+
+import SwiftUI
+
+extension Font {
+    
+    internal static var poppinsBold30px: Font {
+        Font.custom("Poppins-Bold", size: 30)
+    }
+    internal static var poppinsBold24px: Font {
+        Font.custom("Poppins-Bold", size: 24)
+    }
+    internal static var poppinsBold20px: Font {
+        Font.custom("Poppins-Bold", size: 20)
+    }
+    internal static var poppinsBold16px: Font {
+        Font.custom("Poppins-Bold", size: 16)
+    }
+    internal static var poppinsRegular16px: Font {
+        Font.custom("Poppins-Regular", size: 16)
+    }
+    internal static var poppinsRegular14px: Font {
+        Font.custom("Poppins-Regular", size: 14)
+    }
+    internal static var poppinsRegular13px: Font {
+        Font.custom("Poppins-Regular", size: 13)
+    }
+}
