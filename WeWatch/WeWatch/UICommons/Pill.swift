f4556e90a7458574afafd03b287ec88cf2ae7c8e
katasee <kotalov.developer@gmail.com>
2024-12-27 00:54:35 +0100

    WW-11: Add pill components

---
+++ WeWatch/WeWatch/UICommons/Pill.swift
@@ -0,0 +1,18 @@
+//
+//  Pill.swift
+//  WeWatch
+//
+//  Created by Anton on 26/12/2024.
+//
+
+import SwiftUI
+
+struct Pill: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Pill()
+}
