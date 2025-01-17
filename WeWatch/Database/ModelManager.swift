bfa04e564f7a85c111cd403d34657622591b4fd9
Kotalov <kotalov.developer@gmail.com>
2025-01-17 11:30:56 +0100

    WW-13 Database and implement movie data storage (#10)
    * WW-13 Create database and implement movie data storage
    
    * WW-13: Fix issues found during code review
    
    * WW-13: deleting unnecessary files

---
+++ WeWatch/Database/ModelManager.swift
@@ -0,0 +1,35 @@
+//
+//  ModelManager.swift
+//  WeWatch
+//
+//  Created by Anton on 07/01/2025.
+//
+
+import Foundation
+
+internal struct Movie: Identifiable, Sendable {
+    
+    internal var id: Int { movieId }
+    internal var movieId: Int
+    internal var title: String
+    internal var overview: String
+    internal var releaseDate: String
+    internal var rating: Int
+    internal var posterUrl: String
+    
+    internal init(
+        movieId: Int,
+        title: String,
+        overview: String,
+        releaseDate: String,
+        rating: Int,
+        posterUrl: String
+    ) {
+        self.movieId = movieId
+        self.title = title
+        self.overview = overview
+        self.releaseDate = releaseDate
+        self.rating = rating
+        self.posterUrl = posterUrl
+    }
+}
