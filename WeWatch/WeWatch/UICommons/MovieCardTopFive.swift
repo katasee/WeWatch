4c0368a5a66fb386e90df1f63fd958a471ae1cd9
katasee <kotalov.developer@gmail.com>
2025-02-15 10:47:42 +0100

    Fix issues found during code review 3

--- WeWatch/WeWatch/UICommons/MovieCardTopFive.swift
+++ WeWatch/WeWatch/UICommons/MovieCardTopFive.swift
@@ -36,6 +36,9 @@ internal struct MovieCardTopFive: View {
                             image in
                             image
                                 .image?.resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(maxWidth: 300, maxHeight: 200)
+                                .clipped()
                         }
                         Button {
                             isActive.toggle()
@@ -50,7 +53,6 @@ internal struct MovieCardTopFive: View {
                         .padding(16)
                     }
                     .cornerRadius(15)
-                    .frame(maxWidth: 300, maxHeight: 200)
                     .background(RoundedRectangle(cornerRadius: 15)
                         .fill(Color.darkGreyColor))
                 }
@@ -76,17 +78,5 @@ internal struct MovieCardTopFive: View {
         Text(title)
             .font(.poppinsBold20px)
             .foregroundColor(.whiteColor)
-    }
-}
-
-#Preview {
-    ZStack {
-        Color.black.ignoresSafeArea();
-        MovieCardTopFive(
-            title: "Hitman’s Wife’s Bodyguard nfjdfnsjfdn",
-            ranking: 3.5,
-            image: URL(string: "https://miro.medium.com/v2/resize:fit:4800/format:webp/1*39M4XbHXCTfBenNNqLLyLA@2x.jpeg"),
-            didTap: { isActive in }
-        )
     }
 }
