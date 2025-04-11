ea64598f2fb45fdb667158059d1cfea7eee1e0d3
katasee <kotalov.developer@gmail.com>
2025-04-11 13:05:25 +0200

    reduced size of image and tititle in all view

---
+++ WeWatch/WeWatch/ViewModifiers/OrientationObserverModifire.swift
@@ -0,0 +1,18 @@
+//
+//  OrientationObserverModifire.swift
+//  WeWatch
+//
+//  Created by Anton on 11/04/2025.
+//
+
+import SwiftUI
+
+struct OrientationObserverModifire: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrientationObserverModifire()
+}
